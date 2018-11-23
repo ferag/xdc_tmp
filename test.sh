b2cf3847cad1c1a9cb71267d99c36be247be269a
Fernando Aguilar <ferag@live.com>
2018-11-23 14:28:47 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+echo miau > test.txt
 tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
