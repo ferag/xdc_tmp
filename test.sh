4e0ea93b83e4c41a5e1108e0c2263383d821cd03
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-02-05 14:47:53 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 echo miau > test.txt
+sudo su
 tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
