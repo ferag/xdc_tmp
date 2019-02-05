c87739b7865bf1a01e47c37896dff8fbf3fcb5bc
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-02-05 11:13:46 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 # Distributed under the terms of the Modified BSD License.
 echo miau > test.txt
 sudo apt-get install oneclient -y
-sudo tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
+tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
