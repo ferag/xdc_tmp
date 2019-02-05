dce7c3d86b227d7edbbc78a5dc22d061e5b68f83
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-02-05 13:24:26 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,4 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 echo miau > test.txt
-sudo apt-get install oneclient -y
 sudo tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
