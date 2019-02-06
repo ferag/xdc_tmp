f39191028c9cf6a0888f8891d40c6a845060fd33
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-02-06 09:48:39 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@
 # Distributed under the terms of the Modified BSD License.
 echo miau > test.txt
 sudo su
+sudo chown -R jovyan:users /home/jovyan/.local/share/jupyter
 nohup tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888 &
 oneclient --authentication token --no_check_certificate ./datasets
