238f8c3112c9d138a485daff1771deae8aaee38d
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-02-06 09:19:50 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 echo miau > test.txt
-exec tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888 &
+sudo su
+tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888 &
 exec oneclient --authentication token --no_check_certificate ./datasets
