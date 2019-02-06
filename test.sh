90f0232635e82958c29cdd61935b2d7d3c279a02
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-02-06 08:19:34 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 # Distributed under the terms of the Modified BSD License.
 echo miau > test.txt
 exec oneclient --authentication token --no_check_certificate ./datasets
-exec tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
+tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
