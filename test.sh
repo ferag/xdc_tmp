781779a740eb4a3445f22f4fb50000510a08c604
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-02-06 12:35:31 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,9 @@
 # Distributed under the terms of the Modified BSD License.
 echo miau > test.txt
 sudo chown -R jovyan:users /home/jovyan/.local/share/jupyter
+sudo chown -R jovyan:users /home/jovyan/datasets
 whoami
 su root
 whoami
-#exec oneclient --authentication token --no_check_certificate ./datasets
+sudo -u jovyan oneclient --authentication token --no_check_certificate ./datasets
 tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
