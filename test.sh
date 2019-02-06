a2027fcfa46b5182cd240f931e314440026c0980
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-02-06 08:28:46 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@
 # Distributed under the terms of the Modified BSD License.
 echo miau > test.txt
 exec oneclient --authentication token --no_check_certificate ./datasets
+echo "Onedata is mounted"
 tini -g -- start-notebook.sh --ip="0.0.0.0" --port=8888
