8473e6e92c6b627fc33a8897dfdcaa2fc3eeca9f
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-11-20 12:01:32 +0100

    Update waiting.sh

--- waiting.sh
+++ waiting.sh
@@ -3,7 +3,7 @@
 
 cmd="wget https://raw.githubusercontent.com/ferag/xdc_tmp/master/run_test.sh && /bin/bash run_test.sh"
 
-until ls /datasets/CdP/model_2012-01-01_2012-01-03 '\q'; do
+until ls /datasets/CdP/model_2012-01-01_2012-01-03; do
   >&2 echo "Model is unavailable - sleeping"
   sleep 10
 done
