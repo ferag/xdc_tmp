2bef92fe695c6409c4dc785a82ad51d320df2065
Fernando Aguilar <aguilarf@ifca.unican.es>
2019-11-20 11:49:38 +0100

    Create waiting.sh

---
+++ waiting.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# waitint.sh
+
+host="$1"
+shift
+cmd="wget https://raw.githubusercontent.com/ferag/xdc_tmp/master/run_test.sh && /bin/bash run_test.sh"
+
+until ls /datasets/CdP/model_2012-01-01_2012-01-03 '\q'; do
+  >&2 echo "Model is unavailable - sleeping"
+  sleep 10
+done
+
+>&2 echo "Model is ready - executing command"
+exec $cmd
